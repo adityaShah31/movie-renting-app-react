e758175a94d1bcce9819e7db7f9d24548416c827
Aditya Shah <adityajshah31@gmail.com>
2021-07-10 15:38:45 -0400

    test commit 6

--- server.js
+++ server.js
@@ -2,13 +2,12 @@ const jsonServer = require('json-server');
 const server = jsonServer.create();
 
 const express = require('express');
-const path = require('path');
 
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 const PORT = 3000;
 
 server.use('/api', middlewares, router);
-server.use(express.static(path.join(__dirname, 'build')));
+server.use(express.static('build'));
 
 server.listen(PORT, () => console.log(`json-server is listening on Port: ${PORT}`));
