828798c9a48e8d4abe190a422f9fa60cd47fc57d
Aditya Shah <adityajshah31@gmail.com>
2021-07-10 13:49:46 -0400

    Navbar toggler fix

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ const Header = () => {
           <button className='navbar-toggler' type='button' data-bs-toggle='collapse' data-bs-target='#navbarText' aria-controls='navbarText' aria-expanded='false' aria-label='Toggle navigation'>
             <span className='navbar-toggler-icon'></span>
           </button>
-          <div className='collapse navbar-collapse' id='navbarText'>
+          <div className='collapse navbar-collapse bg-dark' id='navbarText'>
             <ul className='navbar-nav me-auto mb-2 mb-lg-0'>
               <li className='nav-item'>
                 <NavLink
